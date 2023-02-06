11a90d823dd813b4c8f66fb0690fc91169414633
Ansi_Nitro <15asktt@gmail.com>
2023-02-06 09:11:31 +0600

    method for light and fields "square" and "height" was added into "Room" abstract class

--- src/Rooms/Room.java
+++ src/Rooms/Room.java
@@ -1,4 +1,16 @@
 package Rooms;
 
 public abstract class Room {
+    private double square;
+    private double height;
+
+    public void switchOffLight(){
+        System.out.println("The light was switched off.");
+    }
+
+    public void switchOnLight(){
+        System.out.println("The light was switched on.");
+    }
+
+
 }
