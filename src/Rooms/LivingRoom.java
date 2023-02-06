7e1f7820c3a5a817661e75b1860e111677d93ecd
Ansi_Nitro <15asktt@gmail.com>
2023-02-06 09:24:29 +0600

    added getters and setters and started work on "living Room"

--- src/Rooms/LivingRoom.java
+++ src/Rooms/LivingRoom.java
@@ -1,4 +1,6 @@
 package Rooms;
 
 public class LivingRoom extends Room {
+    private int numberOfBed;
+    private int numberOfRadiator;
 }
--- src/Rooms/Room.java
+++ src/Rooms/Room.java
@@ -3,6 +3,7 @@ package Rooms;
 public abstract class Room {
     private double square;
     private double height;
+    private int numberOfSockets;
 
     public void switchOffLight(){
         System.out.println("The light was switched off.");
@@ -10,5 +11,29 @@ public abstract class Room {
 
     public void switchOnLight(){
         System.out.println("The light was switched on.");
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setNumberOfSockets(int numberOfSockets) {
+        this.numberOfSockets = numberOfSockets;
+    }
+
+    public int getNumberOfSockets() {
+        return numberOfSockets;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public double getSquare() {
+        return square;
     }
 }
